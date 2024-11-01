2e5c6f20e6cd767fc97f9218d372484c9aa10d95
Fariha <farihaorna@gmail.com>
2024-10-31 21:51:36 -0400

    Implemented media queries to adjust game elements for all major mobile screen sizes

---
+++ script.js
@@ -0,0 +1,7 @@
+// function updateSize() {
+//     document.getElementById('width').textContent = window.innerWidth;
+//     document.getElementById('height').textContent = window.innerHeight;
+// }
+
+// window.addEventListener('resize', updateSize);
+//updateSize(); // Call once to set initial size
