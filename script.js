e14fc45763577d1bde7d41a16a3f15b4ec27851b
Fariha <farihaorna@gmail.com>
2024-11-03 16:54:08 -0500

    rename game, clean up code, and improve responsive styling

--- script.js
+++
@@ -1,7 +0,0 @@
-// function updateSize() {
-//     document.getElementById('width').textContent = window.innerWidth;
-//     document.getElementById('height').textContent = window.innerHeight;
-// }
-
-// window.addEventListener('resize', updateSize);
-// updateSize(); // Call once to set initial size
