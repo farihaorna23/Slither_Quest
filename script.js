6ae6b0d44dbbcbe4e4fa8f1730142329665b93a9
Fariha <farihaorna@gmail.com>
2024-11-04 15:43:27 -0500

    created move funtions so that snake can move around the game board and also added event listener and implemented its handler function to set the direction of the snake based on the user input

--- script.js
+++ script.js
@@ -1,11 +1,15 @@
 // DOM Elements
 const gameBoard = document.querySelector('.game-board')
+const currentScore = document.querySelector('.current-score')
+
 
 //Variables
 const snakePositions = [{x:10,y:10}] //the snake position in the begining of the game is the center
-const snakeDirection = "top" //the snake will default by moving to the top
+const snakeDirection =  "ArrowUp" //the snake will default by moving to the top
 const gameStarted = false //keeps track of whether the game has started or not
 const foodPosition = generateFood() //will hold the position of the food on the gameboard
+const user_current_score = 0 //keeps track of user's score
+
 
 //This function will create each snake segment based on the snakePosition array and set its position
 const createSnake = () => {
@@ -39,7 +43,71 @@ const createFood = () => {
     gameBoard.appendChild(food)
 }
 
-createFood();
+//This function would update the current score based on the speed of the snake
+const updateCurrentScore = () => {
+    let user_score = String(user_current_score)
+    currentScore.textContent = user_score.padStart(3,"0")
+}
+
+//the move function would move the snake around the gameboard based on the user's input
+const move = (direction) => {
+    //copy of the head of the snake
+    let head = {...snakePositions[0]}
+
+    //based on the user's input, the switch case would change the position of the new head of the snake
+    switch(direction){
+        case "ArrowUp":
+            head.x += 1
+            break
+        case "ArrowDown":
+            head.x -= 1
+            break
+        case "ArrowLeft":
+            head.y -= 1
+            break
+        case "ArrowRight":
+            head.y += 1
+            break
+    }
+
+    //it would add the new head to the snake aka SnakePositions array
+    snakePositions.unshift(head)
+    
+    //if user hasn't eaten any food or collided with itself or the border, 
+    //and pop the previous head to give the sense of the snake moving
+    snakePositions.pop()
+}
+
+const userInputHandler = (e) => {
+    //getting the user input
+    let key = e.code
+    //if space bar is pressed, we start the game 
+    //if any of the direction keys are pressed, we would call the move function
+    switch(key){
+        case "Space":
+            gameStarted = true
+            startGame()
+            break
+        case "ArrowUp":
+            snakeDirection = "ArrowUp"
+            break
+        case "ArrowDown":
+            snakeDirection = "ArrowDown"
+            break
+        case "ArrowLeft":
+            snakeDirection = "ArrowLeft"
+            break
+        case "ArrowRight":
+            snakeDirection = "ArrowRight"
+            break
+    }
+}
+
+
+//adding event listener to the window to specifically listen for space bar being pressed by the user
+window.addEventListener("keydown",userInputHandler)
+
+
 
 
 
